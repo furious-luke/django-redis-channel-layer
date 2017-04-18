61942fc2048588e5899defced930e7b4890825a6
Luke Hodkinson <furious.luke@gmail.com>
2017-04-19 06:20:57 +1000

    Support latest asgi_redis

--- redis_channel_layer/core.py
+++ redis_channel_layer/core.py
@@ -3,5 +3,5 @@ from django_redis import get_redis_connection
 
 
 class DjangoRedisChannelLayer(RedisChannelLayer):
-    def _generate_connections(self):
+    def _generate_connections(self, **redis_kwargs):
             return [get_redis_connection()]
